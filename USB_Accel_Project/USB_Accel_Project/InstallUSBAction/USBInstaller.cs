6fded59f0fd2192d0a8ffe7026146ae7fc84957a
Andrew Thornton <andrewthornton1351@gmail.com>
2014-08-12 23:16:35 -0400

    USB Installer Project initial commit.

---
+++ USB_Accel_Project/USB_Accel_Project/InstallUSBAction/USBInstaller.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Diagnostics;
+
+
+namespace InstallUSBAction
+{
+    [RunInstaller(true)]
+    public partial class USBInstaller : System.Configuration.Install.Installer
+    {
+        public USBInstaller()
+        {
+            InitializeComponent();
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+
+            startInfo.FileName = "C:\\Windows\\SysWOW64\\rundll32.exe";
+        
+            string filepath = this.Context.Parameters["SrcDir"];
+
+            string fullInf = filepath + "USB_AUDIO__CODEC.inf";
+
+            startInfo.Arguments = "SETUPAPI.DLL,InstallHinfSection DefaultInstall 132 " + fullInf.ToString();
+
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+    }
+}
